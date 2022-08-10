ed207215e63000a8361222332656a349c8080df9
Kevin Mason <krmason2020@gmail.com>
2022-08-09 21:25:20 -0700

    basic backend working

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5001;
+
+const connectDB = require('./config/db');
+
+//connect to db -- not connecting?? check IP on mongodb atlas or user credentials
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); //to decode body - x-www-form-urlencoded
+// able to recieve req.body.
+
+app.get('/', (req, res) => {
+  res.json({ message: 'hello world' });
+});
+
+app.listen(port, () => {
+  console.log(`Server Started on port:${port}`);
+});
