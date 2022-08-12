f7e9dc8471491c055cf134e036f40d10f9fe18e9
Kevin Mason <krmason2020@gmail.com>
2022-08-11 22:09:18 -0700

    created basic redux toolkit react app, cleared all  useless files

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../middleware/authMiddleware');
+// const { protect } = require('../middleware/authMiddleware');
 const {
   registerUser,
   loginUser,
-  getMe,
   activateUser,
 } = require('../controllers/userController');
 
 router.post('/', registerUser);
 router.post('/login', loginUser);
 router.get('/confirm/:emailConfirmationCode', activateUser);
-router.get('/me', protect, getMe);
 
 module.exports = router;
