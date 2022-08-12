237e464331a8970dbb7c6b7edbcd82cc7c58c2a2
Kevin Mason <krmason2020@gmail.com>
2022-08-12 16:53:36 -0700

    added font spartan

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from './Register.module.css';
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div>
+      <h1>Spartan</h1>
+    </div>
+  );
 };
 
 export default Register;
