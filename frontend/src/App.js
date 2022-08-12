dd3caa63e6b5205ebd929c247f593d801c68c646
Kevin Mason <krmason2020@gmail.com>
2022-08-12 15:51:17 -0700

    created basic frontend react routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import styles from './App.module.css';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>test</h1>
-      Empty app
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
