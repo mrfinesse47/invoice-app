05b2524acc580979c05fe37b39d7debccbf49dfa
Kevin Mason <krmason2020@gmail.com>
2022-08-13 13:28:22 -0700

    begin global styles

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import styles from './Login.module.css';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className={styles.outerContainer}>
+      <section>
+        <h1>Login</h1>
+        <p className='sub-heading'>Please Sign In</p>
+      </section>
+
+      <section className='form'>
+        <form>
+          <div className='form-group'>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={''}
+              placeholder='Enter Your Email'
+              onChange={''}
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={''}
+              placeholder='Enter Your password'
+            />
+          </div>
+
+          <div className='form-group'>
+            <button type='submit' className='btn btn-default'>
+              Save Changes
+            </button>
+          </div>
+        </form>
+      </section>
+    </div>
+  );
 };
 
 export default Login;
