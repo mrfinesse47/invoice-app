9126d255f188f4b36d2ce9b15b339716a6bb8cc5
Kevin Mason <krmason2020@gmail.com>
2022-08-13 23:17:16 -0700

    register route working

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import styles from './Login.module.css';
 import { useState } from 'react';
 import { toast } from 'react-toastify';
+import { register, acknowledgeError } from '../features/auth/authSlice';
 import 'react-toastify/dist/ReactToastify.css';
 
 import Spinner from '../components/Spinner/Spinner';
 
 const Register = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [formData, setFormData] = useState({
     email: '',
     password: '',
+    password2: '',
     userName: '',
   });
 
-  const { email, password, userName } = formData;
+  const { email, password, password2, userName } = formData;
 
   const onChange = (e) => {
     setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
@@ -24,11 +29,45 @@ const Register = () => {
     (state) => state.auth
   );
 
-  const showToastMessage = () => {
-    toast.success('Success Notification !', {
-      position: toast.POSITION.BOTTOM_RIGHT,
-      className: 'toast-message-dark',
-    });
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (password !== password2) {
+      return toast.error('Error: passwords do not match', {
+        className: 'toast-message-dark',
+      });
+    }
+    dispatch(register({ name: userName, email, password }));
+  };
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message, {
+        onClose: () => {
+          dispatch(acknowledgeError());
+        },
+        className: 'toast-message-dark',
+      });
+    }
+    if (user) {
+      navigate('/');
+    }
+    // dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  const showToastMessage = (message, type) => {
+    if (type === 'success') {
+      toast.success(message, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+        className: 'toast-message-dark',
+      });
+    }
+    if (type === 'error') {
+      toast.error(message, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+        className: 'toast-message-dark',
+      });
+    }
   };
 
   return (
@@ -76,15 +115,24 @@ const Register = () => {
               onChange={onChange}
             />
           </div>
+          <div className='form-group'>
+            <label htmlFor='password2'>Confirm Password</label>
+            <input
+              type='password'
+              className='form-control'
+              id='password2'
+              name='password2'
+              autoComplete='password'
+              value={password2}
+              onChange={onChange}
+            />
+          </div>
 
           <div className='form-group'>
             <button
               type='submit'
               className={`btn btn-default ${styles.buttonSubmit}`}
-              onClick={(e) => {
-                e.preventDefault();
-                showToastMessage();
-              }}
+              onClick={onSubmit}
             >
               Register
             </button>
