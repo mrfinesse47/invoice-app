5506b095516383b4e2e9e50d20bdba56aea256ba
Kevin Mason <krmason2020@gmail.com>
2022-08-20 14:59:53 -0700

    more styling to the toolbar

--- frontend/src/components/DropDown/DropDown.jsx
+++ frontend/src/components/DropDown/DropDown.jsx
@@ -3,7 +3,7 @@ import styles from './DropDown.module.css';
 
 const DropDown = () => {
   return (
-    <button>
+    <button className={styles.dropDown}>
       Filter <img src='./icon-arrow-down.svg' alt='filter' />{' '}
     </button>
   );
