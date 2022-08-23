33616477f4ad679e8bf24d4f332d4c6ce6d225a9
Kevin Mason <krmason2020@gmail.com>
2022-08-22 18:41:52 -0700

    created an nothing component

--- frontend/src/components/Invoices/InvoiceList.jsx
+++ frontend/src/components/Invoices/InvoiceList.jsx
@@ -3,21 +3,27 @@ import Invoice from './Invoice';
 import styles from './InvoiceList.module.css';
 import InvoiceToolBar from './InvoiveToolbar';
 import Spinner from '../Spinner/Spinner';
+import Nothing from '../Nothing/Nothing';
 
-const InvoiceList = ({ invoices, isLoading }) => (
-  //invoice toolbar
-  <>
-    <InvoiceToolBar count={invoices.length} />
-    {isLoading ? (
-      <Spinner />
-    ) : (
-      <ul className={styles.invoices}>
-        {invoices.map((invoice) => (
-          <Invoice key={invoice._id} invoice={invoice} />
-        ))}
-      </ul>
-    )}
-  </>
-);
+const InvoiceList = ({ invoices, isLoading }) => {
+  if (isLoading) {
+    return <Spinner />;
+  }
+  return (
+    //invoice toolbar
+    <>
+      <InvoiceToolBar count={invoices.length} />
+      {invoices.length === 0 ? (
+        <Nothing />
+      ) : (
+        <ul className={styles.invoices}>
+          {invoices.map((invoice) => (
+            <Invoice key={invoice._id} invoice={invoice} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
 
 export default InvoiceList;
--- frontend/src/components/Nothing/Nothing.jsx
+++ frontend/src/components/Nothing/Nothing.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styles from './Nothing.module.css';
 
 const Nothing = () => {
-  return <div>Nothing</div>;
+  return (
+    <div className={styles.container}>
+      <img src='./illustration-empty.svg' alt='nothing here' />
+      <div>
+        <h2>There is nothing here</h2>
+        <p className='body-one'>
+          Create a new invoice by clicking the <strong>New Invoice</strong>{' '}
+          button and get started
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Nothing;
